d74db47c5e2c99d4390c4dad8e79547ff0ecd7bf
Nilkantha452 <nilkanthabhattacharjee452@gmail.com>
2024-05-18 02:03:19 +0530

    Do a inorder traversal in a binary tree (iterative and recursive).

---
+++ Problems/Binary Tree/InOrderTraversal.cpp
@@ -0,0 +1,90 @@
+/* Do an inorder traversal in a binary tree. */
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode()
+    {
+        val = 0;
+        left = nullptr;
+        right = nullptr;
+    }
+    TreeNode(int value)
+    {
+        val = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+/* Recursive way to do inorder traversal. */
+void inorderTraversal(TreeNode* root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inorderTraversal(root->left);
+    std::cout << root->val << " ";
+    inorderTraversal(root->right);
+}
+
+std::vector<int> inorderTraversalEx(TreeNode* root)
+{
+    std::stack<TreeNode*> st;
+    std::vector<int> ans;
+    while (1)
+    {
+        if (root != nullptr)
+        {
+            st.push(root);
+            root = root->left;
+        }
+        else
+        {
+            if (st.size() == 0)
+            {
+                break;
+            }
+            TreeNode* node = st.top();
+            ans.push_back(node->val);
+            st.pop();
+            root = node->right;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    TreeNode* root = new TreeNode(3);
+    TreeNode* rl = new TreeNode(1);
+    TreeNode* rr = new TreeNode(2);
+    root->left = rl;
+    root->right = rr;
+    TreeNode* rll = new TreeNode(4);
+    TreeNode* rlr = new TreeNode(5);
+    root->left->left = rll;
+    root->left->right = rlr;
+    TreeNode* rrl = new TreeNode(7);
+    TreeNode* rrr = new TreeNode(6);
+    root->right->left = rrl;
+    root->right->right = rrr;
+    TreeNode* rrlr = new TreeNode(8);
+    root->right->left->right = rrlr;
+
+    inorderTraversal(root);
+    std::cout << std::endl;
+    std::vector<int> ans = inorderTraversalEx(root);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        std::cout << ans[i] << " ";
+    }
+    return 0;
+}
